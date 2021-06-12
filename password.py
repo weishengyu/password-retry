83812ef7d6bf6d54f98b9c41941fb2e570614208
Weisheng Yu <wei91627@gmail.com>
2021-06-12 20:28:55 +0800

    basic version

---
+++ password.py
@@ -0,0 +1,13 @@
+password = 'a123456'
+ｉ= 3 #剩餘機會
+while True:
+	pwd = input('請輸入密碼：')
+	if pwd == password:
+		print('登入成功！')
+		break #輸入對了逃出迴圈
+	else:
+		i = i - 1
+		print('密碼錯誤！還有', ｉ, '次機會' )
+		if i == 0:
+			break
+
